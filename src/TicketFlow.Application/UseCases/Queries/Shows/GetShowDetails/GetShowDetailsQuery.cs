a5f40830469bdbb70639a5cf8d0f2a08a37401bd
kauatwn <kaualopescontato@gmail.com>
2026-01-27 12:32:17 -0300

    feat: implement GetShowDetails query and handler with response model

---
+++ src/TicketFlow.Application/UseCases/Queries/Shows/GetShowDetails/GetShowDetailsQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace TicketFlow.Application.UseCases.Queries.Shows.GetShowDetails;
+
+public record GetShowDetailsQuery(Guid ShowId) : IRequest<ShowDetailsResponse?>;
---
+++ src/TicketFlow.Application/UseCases/Queries/Shows/GetShowDetails/ShowDetailsResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace TicketFlow.Application.UseCases.Queries.Shows.GetShowDetails;
+
+public record ShowDetailsResponse(
+    Guid Id,
+    string Title,
+    DateTime Date,
+    string Status,
+    int TotalTickets,
+    int AvailableTickets);
