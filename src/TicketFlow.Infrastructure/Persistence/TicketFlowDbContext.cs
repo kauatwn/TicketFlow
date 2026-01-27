f6672a202d733f36f8ce9b79111885abe07298d1
kauatwn <kaualopescontato@gmail.com>
2026-01-27 13:23:09 -0300

    feat: add TicketFlowDbContext and TicketFlowSeeder for database seeding

---
+++ src/TicketFlow.Infrastructure/Persistence/TicketFlowDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketFlow.Domain.Entities;
+
+namespace TicketFlow.Infrastructure.Persistence;
+
+public class TicketFlowDbContext(DbContextOptions<TicketFlowDbContext> options) : DbContext(options)
+{
+    public DbSet<Ticket> Tickets { get; set; }
+    public DbSet<Show> Shows { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TicketFlowDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+}
