74d8ba774b4dc1cd81fd1ee1fd606a2b96861288
kauatwn <kaualopescontato@gmail.com>
2026-01-27 12:27:50 -0300

    feat: add repository interfaces for Show, Ticket, and UnitOfWork

---
+++ src/TicketFlow.Domain/Repositories/ITicketRepository.cs
@@ -0,0 +1,9 @@
+﻿using TicketFlow.Domain.Entities;
+
+namespace TicketFlow.Domain.Repositories;
+
+public interface ITicketRepository
+{
+    Task<Ticket?> GetByIdWithShowAsync(Guid id, CancellationToken cancellationToken);
+    void Update(Ticket ticket);
+}
