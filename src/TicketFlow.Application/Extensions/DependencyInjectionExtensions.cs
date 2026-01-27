f08c0f3309f19ccaee59339976bc5b35adee9cbe
kauatwn <kaualopescontato@gmail.com>
2026-01-27 12:39:24 -0300

    feat: add dependency injection extensions for application services

---
+++ src/TicketFlow.Application/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
+using TicketFlow.Application.Behaviors;
+
+namespace TicketFlow.Application.Extensions;
+
+[ExcludeFromCodeCoverage(Justification = "Pure dependency injection configuration")]
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        AddValidation(services);
+
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(typeof(DependencyInjectionExtensions).Assembly);
+
+            configuration.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        });
+
+        return services;
+    }
+
+    private static void AddValidation(IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(typeof(DependencyInjectionExtensions).Assembly);
+    }
+}
